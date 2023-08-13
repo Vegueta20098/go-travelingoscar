fbb65305dcdeb0a0fe447406a70d457eb14fe160
Vegueta20098 <est.orosalesr@uml.edu.ni>
2023-08-12 18:12:44 -0600

    resolver

--- src/pages/Hotels.tsx
+++ src/pages/Hotels.tsx
@@ -1,40 +1,53 @@
-import { Link } from "react-router-dom"
-import { Title } from "../components/Title"
-import { useEffect, useState } from "react"
+import { Link } from "react-router-dom";
+import { Title } from "../components/Title";
+import { useEffect, useState } from "react";
+
+type Hotel = {
+  id: number;
+  name: string;
+  image: string;
+  price: number;
+  starring: number;
+};
 
 export const Hotels = () => {
-  const [hotels, setHotels] = useState([])
+  const [hotels, setHotels] = useState<Hotel[]>([]);
   useEffect(() => {
-    fetch('https://api-tests.workingpos.com/api/go-traveling/hotels')
+    fetch("https://api-tests.workingpos.com/api/go-traveling/hotels")
       .then((response) => response.json())
-      .then((data) => setHotels(data.data))
-  }, [])
- 
+      .then((data) => setHotels(data.data));
+  }, []);
+
   return (
-    <div >
-      <Link to="/posts">
-       <Title texto="Busca un hotel cerca de ti" />
+    <div>
+      
+        <Title texto="Busca un hotel cerca de ti" />
         <div className="grid grid-cols-1 gap-4 mt-4 sm:grid-cols-2">
-          {hotels.map((hotel) => (
-            <div key={hotel.name} className="overflow-hidden bg-white rounded-lg shadow-md hover:cursor-pointer">
-              <img src={hotel.image} alt={hotel.name} className="object-cover object-center w-full h-56" />
+          {hotels.map((hotel :Hotel) => (
+            <Link to={`/posts/${hotel.id}`} key={hotel.id}> 
+            <div
+              className="overflow-hidden bg-white rounded-lg shadow-md hover:cursor-pointer"
+            >
+              <img
+                src={hotel.image}
+                alt={hotel.name}
+                className="object-cover object-center w-full h-56"
+              />
               <div className="py-4">
-                <h3 className="flex justify-between mb-1 text-xs tracking-widest text-gray-500 title-font">{hotel.name}
-                <span>{hotel.starring} estrellas</span>
+                <h3 className="flex justify-between mb-1 text-xs tracking-widest text-gray-500 title-font">
+                  {hotel.name}
+                  <span>{hotel.starring} estrellas</span>
                 </h3>
                 <p className="mt-1">${hotel.price}</p>
                 <div className="flex items-center">
-                  
-                  <span className="flex items-center py-1 pr-3 text-sm leading-none text-gray-400">
-                    
-                  </span>
-                  </div>
-                
+                  <span className="flex items-center py-1 pr-3 text-sm leading-none text-gray-400"></span>
+                </div>
               </div>
             </div>
+            </Link>
           ))}
-          </div>
-          </Link>
+        </div>
+    
     </div>
-  )
-}
+  );
+};
--- src/pages/Posts.tsx
+++ src/pages/Posts.tsx
@@ -68,3 +68,4 @@ export const Posts = () => {
     </div>
   );
 };
+
